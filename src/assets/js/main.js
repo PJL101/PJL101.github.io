435d1f4354ec200f81f1854cf174f9a2046b1154
Phil Lennon <phil@iampjl.co.uk>
2015-07-01 00:17:16 +0100

    initial styling & content

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,7 @@
 // Load required modules
 // exampleModule.init();
 //console.log('in main file: ' + exampleModule.publicVar);
+
+var html = document.querySelector('html');
+html.classList.remove('no-js');
+html.classList.add('js');
