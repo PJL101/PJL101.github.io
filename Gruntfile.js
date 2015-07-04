b65215a98cda1715c1b2c5e421946b388a1ce48c
Phil Lennon <phil@iampjl.co.uk>
2015-07-04 14:39:27 +0100

    add bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -318,7 +318,28 @@ module.exports = function(grunt) {
           watchTask: true
         }
       }
-    }
+    },
+
+    // Bump task
+    // Use by running: 'grunt bump:patch', 'grunt bump:minor', 'grunt bump:major'
+    bump: {
+        options: {
+          files: ['package.json'],
+          updateConfigs: [],
+          commit: true,
+          commitMessage: 'Release v%VERSION%',
+          commitFiles: ['package.json'],
+          createTag: true,
+          tagName: 'v%VERSION%',
+          tagMessage: 'Version %VERSION%',
+          push: true,
+          pushTo: 'origin',
+          gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+          globalReplace: false,
+          prereleaseName: false,
+          regExp: false
+        }
+      },    
   });
 
   // Tasks
